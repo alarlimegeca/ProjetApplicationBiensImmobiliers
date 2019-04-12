9e233d1cf58b789e24563381dd31ac3eb84346e3
alarlimegeca <49456352+alarlimegeca@users.noreply.github.com>
2019-04-12 11:56:11 +0200

    Ajout classe connexion

---
+++ Connexion.java
@@ -0,0 +1,38 @@
+package individus;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Connexion {
+	
+	static final String DB_URL = "jdbc:sqlite:/media/formation/CLEF MENGIN/Projet info/BDD/Individus.db";
+	static final String DB_USER = "dbase";
+	static final String DB_PASS = "dbase2019";
+	
+	static Connection connection;
+	
+	/**
+	 * Constructeur
+	 * 
+	 */
+	
+	private Connexion (){
+		try{
+			Class.forName("org.sqlite.JDBC");
+			Connexion.connection = DriverManager.getConnection(DB_URL,DB_USER,DB_PASS);
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Récupére ou construit si elle n'existe pas l'instance connexion
+	 * @return
+	 */
+	public static Connection getinstance(){
+		if (Connexion.connection == null){
+			new Connexion();	
+		}
+		return Connexion.connection;
+	}
+}
