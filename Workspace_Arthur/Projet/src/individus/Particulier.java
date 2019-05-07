3969ec886fafbf6b4ced89237449268318091f11
alarlimegeca <49456352+alarlimegeca@users.noreply.github.com>
2019-05-07 11:20:16 +0200

    maj prise_rdv

--- Workspace_Arthur/Projet/src/individus/Particulier.java
+++ Workspace_Arthur/Projet/src/individus/Particulier.java
@@ -9,6 +9,37 @@ public class Particulier extends Individu{
 		
 	}
 	
+	public static ArrayList choisirBien() throws ClassNotFoundException, SQLException{
+		Connection conn = null;
+	    Statement stmt = null;
+	    Statement stmt2 = null;
+	    Class.forName("org.sqlite.JDBC");
+	    conn = DriverManager.getConnection("jdbc:sqlite:bdd.db");
+	    stmt = conn.createStatement();
+	    stmt2 = conn.createStatement();
+		ArrayList<String> liste_biens=new ArrayList<>();
+		ArrayList liste_resultat=new ArrayList<>();
+		
+	    ResultSet res = stmt.executeQuery("SELECT * FROM biens_immobiliers WHERE en_ligne =1");
+	    while (res.next()){
+	           String bien = res.getString("nom");
+	           liste_biens.add(bien);
+	    		}
+   	  			String[] liste_biens_simple = new String[ liste_biens.size() ];
+   	  			liste_biens.toArray( liste_biens_simple );
+   	  			String choix_bien = Dialogue.choisirBien(liste_biens_simple);
+   	  			liste_resultat.add(choix_bien);
+   	  			ResultSet res2 = stmt2.executeQuery("SELECT * FROM biens_immobiliers WHERE nom LIKE '"+choix_bien+"'");
+   	  			while (res2.next()){
+   	  				int id_bien = res2.getInt("id_bien");
+   	  				liste_resultat.add(id_bien);
+   	  			}
+	    stmt.close();
+            stmt2.close();
+	    conn.close();
+	    return liste_resultat; 
+
+	 }
 	public static int donnerInfoRdv(){
 		String nom = Dialogue.nom();
 		String prenom = Dialogue.prenom();
